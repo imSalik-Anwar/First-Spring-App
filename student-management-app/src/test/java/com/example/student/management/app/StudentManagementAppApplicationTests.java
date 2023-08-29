c410f145a48fd0272e6ce34c342c6df1fc5cd7d4
Salik Anwar <infernosight@gmail.com>
2023-08-29 16:35:58 +0530

    added unit tests

--- student-management-app/src/test/java/com/example/student/management/app/StudentManagementAppApplicationTests.java
+++ student-management-app/src/test/java/com/example/student/management/app/StudentManagementAppApplicationTests.java
@@ -1,11 +1,43 @@
 package com.example.student.management.app;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudentManagementAppApplicationTests {
+	@Autowired
+	StudentService studentService;
 
+	@BeforeEach
+	public void preOperation(){
+		// regNo, name, course, age
+		Student s1 = new Student(1, "Salik", "BBA", 28);
+		Student s2 = new Student(2, "Kalam", "BCOM", 27);
+		Student s3 = new Student(3, "Ateeque", "BA", 26);
+	}
+	@Test
+	public void addStudentTest(){
+		Student expected = new Student(2, "Kalam", "BCOM", 27);
+		Student output = studentService.getStudentUsingReqParam(2);
+
+		Assertions.assertEquals(expected.getRegNo(), output.getRegNo());
+		Assertions.assertEquals(expected.getName(), output.getName());
+		Assertions.assertEquals(expected.getCourse(), output.getCourse());
+		Assertions.assertEquals(expected.getAge(), output.getAge());
+	}
+	@Test
+	public void getStudentTest(){
+		Student expected = new Student(2, "Kalam", "BCOM", 27);
+		Student output = studentService.getStudentUsingReqParam(2);
+
+		Assertions.assertEquals(expected.getRegNo(), output.getRegNo());
+		Assertions.assertEquals(expected.getName(), output.getName());
+		Assertions.assertEquals(expected.getCourse(), output.getCourse());
+		Assertions.assertEquals(expected.getAge(), output.getAge());
+	}
 	@Test
 	void contextLoads() {
 	}
